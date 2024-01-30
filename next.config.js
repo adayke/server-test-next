b68e8deca6c7f888c0fb17dfe8b1f53da92f7dba
Aday Kagazbek <aday@eventum.one>
2024-01-30 13:29:11 +0600

    nm

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/server-test-react",
+  basePath: "/server-test-next",
+  assetPrefix: "/server-test-next",
 };
 
 module.exports = nextConfig;
